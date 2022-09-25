33cd36070e72e5b8b39ada58be631deadc661314
Acrylic125 <acrylic125email@gmail.com>
2022-09-25 12:43:36 +0800

    feat: debouncing

--- src/components/search/BigSearch.jsx
+++ src/components/search/BigSearch.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import cx from "classnames";
 import styles from "./BigSearch.module.css";
@@ -9,12 +9,19 @@ export default function BigSearch({ defaultValue, onSearch, style, className }) 
   // React recommends we CONTROL the input, https://reactjs.org/docs/uncontrolled-components.html#:~:text=In%20most%20cases%2C%20we%20recommend,handled%20by%20the%20DOM%20itself.
   const [value, setValue] = useState(defaultValue || "");
 
-  const search = () => {
-    if (typeof onSearch === "function") {
-      onSearch(value);
-    }
-    console.log(value);
-  };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const search = useCallback(
+    debounce((searchValue) => {
+      if (typeof onSearch === "function") {
+        onSearch(searchValue);
+      }
+    }, 1000),
+    []
+  );
+
+  useEffect(() => {
+    search(value);
+  }, [search, value]);
 
   return (
     <div style={style} className={cx(styles.wrapper, className)}>
@@ -28,10 +35,32 @@ export default function BigSearch({ defaultValue, onSearch, style, className }) 
         value={value}
         onChange={(e) => {
           setValue(e.target.value);
-          //   search();
         }}
       />
-      <Button onClick={search}>Search</Button>
+      <Button onClick={() => search(value)}>Search</Button>
     </div>
   );
+}
+
+/**
+ *
+ * @param {T} cb
+ * @param {number} wait
+ * @param {boolean | undefined} immediate
+ * @template {Function} T
+ * @returns {T}
+ */
+function debounce(cb, wait, immediate = false) {
+  return (...args) => {
+    let timeout;
+    const later = () => {
+      timeout = null;
+      if (!immediate) cb(...args);
+    };
+
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) cb(...args);
+  };
 }
